a82882bb891d7c2897628a929973cb65bc4e17df
balpa <berkealtiparmak@outlook.com>
2023-08-27 00:01:09 +0300

    email

--- src/components/contact/ContactContent.jsx
+++ src/components/contact/ContactContent.jsx
@@ -1,43 +1,65 @@
 import '../../App.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser';
 
 function ContactContent() {
+  const form = useRef();
+
   const [email, setEmail] = useState('')
   const [subject, setSubject] = useState('')
   const [content, setContent] = useState('')
+  const [emailSent, setEmailSent] = useState(false)
 
   const emailValidationRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-  const handleSubmit = async () => {
-  }
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!emailSent) {
+      emailjs.sendForm('service_4up0xa5', 'template_kn2ghfh', form.current, `${import.meta.env.VITE_EMAIL_KEY}`)
+      .then((result) => {
+        console.log(result.text);
+
+        setEmailSent(true)
+      }, (error) => {
+        console.log(error.text);
+      });
+    }
+  };
 
   return (
     <div className='contact-content-wrapper'>
         <div className='contact-content-container'>
           <div className='contact-content-box'>
             <div className="contact-content-email-title">Reach out to me!</div>
-            <input
-              className='contact-content-email-input default-input'
-              placeholder='Your E-mail'
-              type='email'
-              maxLength={100}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <input
-              className='contact-content-email-subject-input default-input'
-              placeholder='Subject'
-              type='text'
-              maxLength={100}
-              onChange={(e) => setSubject(e.target.value)}
-            ></input>
-            <textarea
-              className='contact-content-email-content-input'
-              placeholder='Write your email'
-              type='text'
-              maxLength={1000}
-              onChange={(e) => setContent(e.target.value)}
-            ></textarea>
-            <button className='contact-content-email-send-button' onClick={handleSubmit}>Send</button>
+            <form ref={form} onSubmit={sendEmail}>
+              <input
+                className='contact-content-email-input default-input'
+                placeholder='Your E-mail'
+                type='email'
+                name="user_email"
+                maxLength={100}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <input
+                className='contact-content-email-subject-input default-input'
+                placeholder='Subject'
+                type='text'
+                name='email_title'
+                maxLength={100}
+                onChange={(e) => setSubject(e.target.value)}
+              ></input>
+              <textarea
+                className='contact-content-email-content-input'
+                placeholder='Write your email'
+                type='text'
+                name='message'
+                maxLength={1000}
+                onChange={(e) => setContent(e.target.value)}
+              ></textarea>
+              <input type='submit' value='Send' />
+            </form>
+            {/* <button className='contact-content-email-send-button'>Send</button> */}
           </div>
         </div>
     </div>
