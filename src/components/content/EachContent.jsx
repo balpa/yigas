fd69eb163641668c3d689502d59a852062c16489
balpa <berkealtiparmak@outlook.com>
2023-08-26 21:23:12 +0300

    about done

--- src/components/content/EachContent.jsx
+++ src/components/content/EachContent.jsx
@@ -1,12 +1,13 @@
 import '../../App.css'
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 function EachContent({ content }) {
   return (
     <>
     <div className='content-text-wrapper'>
       <div className="content-text-title">{ content.title }</div>
-      <div className="content-text-sub">{ content.text }</div>
+      <div className="content-text-sub">{ ReactHtmlParser(content.text) }</div>
     </div>
     </>
   )
